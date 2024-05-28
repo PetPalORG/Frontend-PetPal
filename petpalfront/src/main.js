422ac55da3f81e5f25ff81924691a6b573b3b3e6
paolomv02 <u202010039@upc.edu.pe>
2024-05-28 02:40:26 -0500

    feat: import primevue components

--- petpalfront/src/main.js
+++ petpalfront/src/main.js
@@ -1,4 +1,45 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config";
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+
+import 'primevue/resources/primevue.min.css'
+import 'primevue/resources/themes/lara-light-amber/theme.css'
+
+
+
+import Password from "primevue/password";
+import Toast from "primevue/toast";
+import FloatLabel from "primevue/floatlabel";
+import ToastService from "primevue/toastservice";
+import Toolbar from "primevue/toolbar";
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Sidebar from 'primevue/sidebar';
+import Card from "primevue/card";
+
+import StyleClass from 'primevue/styleclass';
+import Ripple from "primevue/ripple";
+
+createApp(App)
+    .use(PrimeVue, {ripple: true})
+    .use(router)
+    .component('Button', Button)
+    .component('pv-toolbar', Toolbar)
+    .component('InputText', InputText)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-card', Card)
+    .component('Password', Password)
+    .component('pv-routerLink', router)
+    .component('pv-toast', Toast)
+    .component('pv-toastService', ToastService)
+    .component('pv-floatLabel', FloatLabel)
+
+    .directive('ripple', Ripple)
+    .directive('styleclass', StyleClass)
+
+
+    .mount('#app')
