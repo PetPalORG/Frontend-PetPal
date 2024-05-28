4a55700ddfcd11eca60c66d58838f91d679a62e7
paolomv02 <u202010039@upc.edu.pe>
2024-05-28 03:24:25 -0500

    feat: app

---
+++ petpalfront/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import ToolbarComponent from "@/public/components/toolbar.component.vue";
+export default {
+  name: 'App',
+  components: {ToolbarComponent},
+}
+
+</script>
+
+<template>
+  <toolbar-component v-if="$route.name !== 'login' && $route.name !== 'register' && $route.name !== 'landing'"></toolbar-component>
+  <router-view/>
+</template>
