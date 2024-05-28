bff5cbb42194acd370c16e698fbfc73b8b5836b6
paolomv02 <u202010039@upc.edu.pe>
2024-05-28 03:39:53 -0500

    feat: home page

--- petpalfront/src/router/index.js
+++ petpalfront/src/router/index.js
@@ -1,7 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes= [
-
+    {
+        path: "/home",
+        name: "home",
+        component: () => import("@/public/pages/home/home.vue")
+    },
 ]
 
 const router = createRouter({
