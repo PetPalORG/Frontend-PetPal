ed7861cebce1f6bebc037e2d950031a39613d4dc
cap69n3m0 <u202219315@upc.edu.pe>
2024-05-29 22:57:32 -0500

    feat: Add detail page for articles

---
+++ petpalfront/src/articles/pages/detail/detail.vue
@@ -0,0 +1,13 @@
+<script>
+import ArticleDetail from "@/articles/components/detail.component.vue";
+export default {
+  name: 'detail',
+  components: {ArticleDetail}
+}
+</script>
+
+<template>
+  <div class="container">
+    <article-detail></article-detail>
+  </div>
+</template>
