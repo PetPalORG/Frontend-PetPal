53235bae6eac95fd2948dc5ac45c73d2e2ad3028
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-05-29 22:27:30 -0500

    feature: profile page

---
+++ petpalfront/src/user/pages/profile/profile.vue
@@ -0,0 +1,11 @@
+<script>
+import ProfileComponent from "@/user/components/profile/profile.component.vue";
+export default {
+  name: 'profile',
+  components: {ProfileComponent},
+}
+</script>
+
+<template>
+  <profile-component></profile-component>
+</template>
