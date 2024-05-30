1014902e075df69f3b2efb35521d2e5de2a5aaf2
Michael Carmelino Dueñas <carmelino0213@gmail.com>
2024-05-29 23:52:00 -0500

    feat: create login vue

---
+++ petpalfront/src/user/pages/login/login.vue
@@ -0,0 +1,16 @@
+<script>
+import Login from "@/user/components/login/login.component.vue";
+
+export default {
+  name: "login",
+  components: {Login}
+}
+
+</script>
+
+<template>
+
+  <div class="container">
+    <Login></Login>
+  </div>
+</template>
